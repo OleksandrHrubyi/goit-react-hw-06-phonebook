28a15e0a9858a7941819fc0fb9ce9444f5831bfe
OleksandrGrubyi <ghrubyi@ukr.net>
2021-02-16 11:48:20 +0200

    fix app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,3 @@
-import React, { Component } from "react";
 import PhonebookForm from "../PhonebookForm/PhonebookForm";
 import ContactList from "../ContactList/ContactList";
 import Filter from "../Filter/Filter";
@@ -8,27 +7,25 @@ import { CSSTransition } from "react-transition-group";
 
 import title from "./title.module.css";
 
-class App extends Component {
-  render() {
-    return (
-      <Container>
-        <CSSTransition
-          in={true}
-          classNames={title}
-          timeout={500}
-          appear={true}
-          unmountOnExit
-        >
-          <h1>Phonebook</h1>
-        </CSSTransition>
+function App() {
+  return (
+    <Container>
+      <CSSTransition
+        in={true}
+        classNames={title}
+        timeout={500}
+        appear={true}
+        unmountOnExit
+      >
+        <h1>Phonebook</h1>
+      </CSSTransition>
 
-        <PhonebookForm onSubmit={this.handlerFormSubmit} />
-        <h2>Contacts</h2>
-        <Filter />
-        <ContactList />
-      </Container>
-    );
-  }
+      <PhonebookForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+    </Container>
+  );
 }
 
 export default App;
